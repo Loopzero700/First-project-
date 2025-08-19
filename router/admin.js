8a02d98bc68609f1200d6ce6f5627fbc0dbf0dc1
Looperzero007 <prasantht861@gmail.com>
2025-08-19 23:42:35 +0530

    add admin producet

--- router/admin.js
+++ router/admin.js
@@ -3,5 +3,9 @@ const router = express.Router();
 
 router.get('/login', (req, res) => {res.render('admin/admin-login',
 {title: 'adminLogin',customCss: 'admin-css/login', layout:'layouts/admin'});})
+router.get('/dashboard',(req,res)=>{res.render('admin/admin-dashboard', 
+    {customCss:'admin-css/admin-deshboard',layout:'layouts/admin-layout'})})
+router.get('/producet',(req,res)=>{res.render('admin/admin-produect', 
+    {customCss:'admin-css/admin-produect',layout:'layouts/admin-layout'})})
 
 module.exports = router;
