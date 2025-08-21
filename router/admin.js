02e0fb2de0e144833a0c4b386a03c42db1a3ef81
Looperzero007 <prasantht861@gmail.com>
2025-08-21 10:04:05 +0530

    admin side html and css setup

--- router/admin.js
+++ router/admin.js
@@ -7,5 +7,9 @@ router.get('/dashboard',(req,res)=>{res.render('admin/admin-dashboard',
     {customCss:'admin-css/admin-deshboard',layout:'layouts/admin-layout'})})
 router.get('/producet',(req,res)=>{res.render('admin/admin-produect', 
     {customCss:'admin-css/admin-produect',layout:'layouts/admin-layout'})})
+router.get('/customers',(req,res)=>{res.render('admin/admin-customers', 
+    {customCss:'admin-css/admin-customers',layout:'layouts/admin-layout'})})
+router.get('/orders',(req,res)=>{res.render('admin/admin-orders', 
+    {customCss:'admin-css/admin-orders',layout:'layouts/admin-layout'})})
 
 module.exports = router;
